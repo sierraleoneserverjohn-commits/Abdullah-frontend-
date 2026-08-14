1ab327b1304751e20cc7d7990b90a886393722f7
sierraleoneserverjohn-commits <sierraleoneserverjohn@gmail.com>
2026-08-14 00:49:22 +0000

    Update orb.js

--- js/orb.js
+++ js/orb.js
@@ -1,1 +1,10 @@
+export function initOrb() {
+  const orb = document.getElementById('aiOrb');
+  return {
+    setListening: (isListening) => {
+      if (isListening) orb.classList.add('listening');
+      else orb.classList.remove('listening');
+    }
+  };
+}
 
