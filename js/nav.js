ec770e91c941fe889f3940b5c13f44876b66cf69
sierraleoneserverjohn-commits <sierraleoneserverjohn@gmail.com>
2026-08-14 00:53:55 +0000

    Update nav.js

--- js/nav.js
+++ js/nav.js
@@ -1,1 +1,24 @@
+import { triggerSanaAnimation } from './sana-animation.js';
+
+export function initNavigation() {
+  const navItems = document.querySelectorAll('.nav-item');
+  const centerHeartBtn = document.getElementById('centerHeartBtn');
+
+  navItems.forEach(item => {
+    item.addEventListener('click', () => {
+      navItems.forEach(i => i.classList.remove('active'));
+      item.classList.add('active');
+
+      if (item.dataset.tab === 'sana') {
+        triggerSanaAnimation();
+      }
+    });
+  });
+
+  if (centerHeartBtn) {
+    centerHeartBtn.addEventListener('click', () => {
+      triggerSanaAnimation();
+    });
+  }
+}
 
